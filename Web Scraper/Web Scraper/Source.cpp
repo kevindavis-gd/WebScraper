33b6606820d35804f696c3d0b831784f5f53b8d1
kevindavis-gd <kevindavis.gd@gmail.com>
2020-11-14 01:50:26 -0600

    update

---
+++ Web Scraper/Web Scraper/Source.cpp
@@ -0,0 +1,99 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h> 
+#pragma comment(lib, "Ws2_32.lib")
+int main()
+{
+	WSADATA wsaData;
+	int iResult;
+	// Initialize Winsock 
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0)
+	{
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+
+	struct addrinfo* result = NULL,* ptr = NULL,hints;
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+#define DEFAULT_PORT "80" 
+	// Resolve the server address and port 
+	iResult = getaddrinfo("www.google.com", DEFAULT_PORT, &hints,
+		&result);
+	if (iResult != 0)
+	{
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	SOCKET ConnectSocket = INVALID_SOCKET;
+	// Attempt to connect to the first address returned by 
+	// the call to getaddrinfo
+	ptr = result;
+	// Create a SOCKET for connecting to server 
+	ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+	if (ConnectSocket == INVALID_SOCKET)
+	{
+		printf("Error at socket(): %ld\n",
+			WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+	// Connect to server.
+	iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		closesocket(ConnectSocket);
+		ConnectSocket = INVALID_SOCKET;
+	}
+
+#define DEFAULT_BUFLEN 512 
+	int sendbuflen = DEFAULT_BUFLEN;
+	char sendbuf[DEFAULT_BUFLEN] = "GET\n";
+
+	// Send an initial buffer
+	iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
+	if (iResult == SOCKET_ERROR) {
+		printf("send failed: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+#define DEFAULT_BUFLEN 512 
+	//char* sendbuf = "this is a test";
+	int recvbuflen = DEFAULT_BUFLEN;
+	char recvbuf[DEFAULT_BUFLEN];
+	
+	// Receive data until the server closes the connection
+		do {
+			iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+			if (iResult > 0)
+				printf("Bytes received: %d\n", iResult);
+			else if (iResult == 0)
+				printf("Connection closed\n");
+			else
+				printf("recv failed: %d\n", WSAGetLastError());
+		} while (iResult > 0);
+
+
+	// shutdown the connection for sending since no more data will be sent
+	// the client can still use the ConnectSocket for receiving data
+	iResult = shutdown(ConnectSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed: %d\n", WSAGetLastError());
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// cleanup 
+	closesocket(ConnectSocket);
+	WSACleanup();
+	return 0;
+}
